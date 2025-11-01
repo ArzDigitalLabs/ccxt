dbd837f8a5b687a8f330487ac30dd2a3301fef43
Mo Nash <nasher.themo@gmail.com>
2025-11-01 21:32:32 +0330

    chore: bump version to 4.9.17

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -44,11 +44,11 @@ use React\EventLoop\Loop;
 
 use Exception;
 
-$version = '4.9.16';
+$version = '4.9.17';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '4.9.16';
+    const VERSION = '4.9.17';
 
     public $browser;
     public $marketsLoading = null;
