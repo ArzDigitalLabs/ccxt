9e9bb1bb570680f1ea12fb08f02c3130572559c7
amirhosseinfaghan <www.amir9203joyandeh@gmail.com>
2026-09-26 11:41:52 +0330

    fix: make Iranian exchange adapters Python-compatible

--- js/src/gerami.js
+++ js/src/gerami.js
@@ -52,7 +52,7 @@ export default class gerami extends Exchange {
         });
     }
     async fetchMarkets(params = {}) {
-        const marketIds = ['XAU750g_IRT', 'XAG999g_IRT'];
+        const marketIds = ['XAU750g_IRT', 'XAG999g_IRT', 'XCU9999g_IRT'];
         const result = [];
         for (let i = 0; i < marketIds.length; i++) {
             const response = await this.publicGetPairsPair(this.extend({
@@ -69,6 +69,9 @@ export default class gerami extends Exchange {
         let base = 'XAG-1G';
         if (baseId === 'XAU750g') {
             base = 'XAU18';
+        }
+        if (baseId === 'XCU9999g') {
+            base = 'XCU-1G';
         }
         return {
             'id': this.safeString(data, 'market'),
@@ -124,7 +127,7 @@ export default class gerami extends Exchange {
         if (symbols !== undefined) {
             symbols = this.marketSymbols(symbols);
         }
-        const marketSymbols = ['XAU18/IRT', 'XAG-1G/IRT'];
+        const marketSymbols = ['XAU18/IRT', 'XAG-1G/IRT', 'XCU-1G/IRT'];
         const result = {};
         for (let i = 0; i < marketSymbols.length; i++) {
             const market = this.market(marketSymbols[i]);
--- js/src/melligold.js
+++ js/src/melligold.js
@@ -56,7 +56,7 @@ export default class melligold extends Exchange {
         });
     }
     async fetchMarkets(params = {}) {
-        const symbols = ['XAU18', 'XAG'];
+        const symbols = ['XAU18', 'XAG', 'XCU'];
         const result = [];
         for (let i = 0; i < symbols.length; i++) {
             const response = await this.requestWithCookie(this.extend({
@@ -107,6 +107,9 @@ export default class melligold extends Exchange {
         let base = this.safeCurrencyCode(baseId);
         if (base === 'XAG') {
             base = 'XAG-1G';
+        }
+        if (base === 'XCU') {
+            base = 'XCU-1G';
         }
         const quote = 'IRT';
         return {
@@ -165,7 +168,7 @@ export default class melligold extends Exchange {
             symbols = this.marketSymbols(symbols);
         }
         const result = {};
-        const marketSymbols = ['XAU18/IRT', 'XAG-1G/IRT'];
+        const marketSymbols = ['XAU18/IRT', 'XAG-1G/IRT', 'XCU-1G/IRT'];
         for (let i = 0; i < marketSymbols.length; i++) {
             const market = this.market(marketSymbols[i]);
             const response = await this.requestWithCookie(this.extend({
